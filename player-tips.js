fa9705b334ebae4bed1db83ce3fed3ae8383f9d4
Jessica <j.j.isaacs@hotmail.co.uk>
2022-10-30 20:59:26 +0000

    init

---
+++ player-tips.js
@@ -0,0 +1,32 @@
+const player_tips = {
+    "generic": [
+        "You can bribe the DM",
+        "Running is always an option",
+        "Running is the most reliable method of survival",
+        "Ask for things, please",
+        "Conflict among the party is acceptable. Conflict among the players is not. Don’t make things personal.",
+        "If you are having trouble with an encounter, try reducing the enemies hitpoints to 0, while keeping yours above 0.",
+        "Remember to check your inventory frequently.",
+        "If you don't know what to do on your turn, dodge.",
+        "No matter how high your Persuasion roll is, the King won't give up his kingdom after one conversation",
+        "You also get xp and loot by NOT killing everything you meet",
+        "Bards don't have to be horny",
+        "If you don't know where to go, try asking the locals about recent goings-on!",
+        "If combat is going badly, remember that most humanoids don't enjoy killing and might accept your surrender",
+        "Remember, there are no bad ideas, only unwanted consequences.",
+        "Difficulty in negotiating? Have you tried not being a dick?",
+        "Originality is overrated. Just have fun.",
+        "As a reminder, war crimes are still bad."
+    ],
+    "gameplay": [
+        "Did you know that classes and subclasses will often grant unique and special features that you can actually use?",
+        "Most spells that target objects or creatures require the object or creature to be visible",
+        "Critical success only applies to attack rolls",
+        "The shove action occupies one of your attacks, offering a potentially free disengage during a multiattack"
+    ]
+}
+
+// schools
+// conditions
+//damage types
+//combat rules
